3442e996023f3446ce09cbc13047c5978d2b1dd8
JinaKim77 <G00353420@gmit.ie>
2019-10-02 11:49:59 +0100

    second commit

--- src/components/movieItem.js
+++ src/components/movieItem.js
@@ -3,30 +3,26 @@ import Card from 'react-bootstrap/Card';
 
 class MovieItem extends React.Component {
 
-  render(){
-    return (
-      <div>
-        {/* <h4> {this.props.movie.Title}  </h4>
+    render() {
+        return (
+            <div>
+                {/* <h4> {this.props.movie.Title}  </h4>
           <h4> {this.props.movie.Year}  </h4>
     <img src={this.props.movie.Poster}></img>*/}
 
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>{this.props.movie.Title}</Card.Title>
-    <Card.Text>
-    {this.props.movie.Year}
-    </Card.Text>
-    <img src={this.props.movie.Poster}></img>
-    <footer>
-    {this.props.movie.Poster}
-    </footer>
-  </Card.Body>
-</Card>
-      </div>
-    );
-  }
- 
+                <Card border="dark" bg="warning" style={{ width: '25rem', height: '50rem' }}>
+                    <Card.Img variant="top" src={this.props.movie.Poster} />
+                    <Card.Body>
+                        <Card.Title>{this.props.movie.Title}</Card.Title>
+                        <Card.Text>
+                            {this.props.movie.Year}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+        );
+    }
+
 }
 
 export default MovieItem;
